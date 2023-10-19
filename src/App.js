2e7dc17239f6e2a97626095e4e0789bf256c886f
Zach <wakuwakuzakku@gmail.com>
2023-10-19 15:43:13 +0900

    created header component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import {OrangeButton} from './StyledComponents'
 import SimpleContainer from './OuterContainer';
 import ButtonContainer from './ButtonsContainer';
+import Header from './Header';
 
 
 function App() {
   return (
     <div className="App">
-      < SimpleContainer>
-      <h1>Pomodoro Tomato Counter</h1>
-      <h4>(Made with Material UI this time)</h4>
+      <SimpleContainer>
+      <Header />
       <ButtonContainer>
       <OrangeButton variant='contained'> - </OrangeButton>
       <OrangeButton variant='contained'> + </OrangeButton>
